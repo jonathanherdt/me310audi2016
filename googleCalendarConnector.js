0aedd0608db923e418087cdfada6e761f49b9523
Johanna Latt <johanna.latt@gmx.de>
2016-04-12 14:43:05 +0200

    google calendar only requests events in the coming 7 days to find the next event (to avoid long loading times)

--- googleCalendarConnector.js
+++ googleCalendarConnector.js
@@ -4,26 +4,26 @@ var host = 'https://www.googleapis.com',
 	path_calendar = '/calendar/v3/calendars/primary',
 	path_eventlist = '/events';
 
-exports.getOrdereFutureCalendarEvents = function(http, access_token, callback) {
-	var request_url = host + path_calendar + path_eventlist + '?access_token=' + access_token + '&singleEvents=true&orderBy=startTime&timeMin=' + ISODateString(new Date());
-	
+exports.getOrdereFutureCalendarEvents = function (http, access_token, callback) {
+	var maxTimeToRequest = new Date();
+	maxTimeToRequest.setDate(maxTimeToRequest.getDate() + 7); // request only events that happen in the coming week 
+
+	var request_url = host + path_calendar + path_eventlist + '?access_token=' + access_token + '&singleEvents=true&orderBy=startTime&timeMax=' + ISODateString(maxTimeToRequest) + '&timeMin=' + ISODateString(new Date());
+
 	console.log("Requesting all future events: " + request_url);
 
 	request(request_url, function (error, response, body) {
-	  events = JSON.parse(body).items;
-	  console.log(events);
-	  callback(events);
-	  // callback(JSON.stringify(events));
+		events = JSON.parse(body).items;
+		console.log(events);
+		callback(events);
+		// callback(JSON.stringify(events));
 	});
 }
 
 // helper function to get google formatted date
-function ISODateString(d){
- function pad(n){return n<10 ? '0'+n : n}
- return d.getUTCFullYear()+'-'
-      + pad(d.getUTCMonth()+1)+'-'
-      + pad(d.getUTCDate())+'T'
-      + pad(d.getUTCHours())+':'
-      + pad(d.getUTCMinutes())+':'
-      + pad(d.getUTCSeconds())+'Z'
+function ISODateString(d) {
+	function pad(n) {
+		return n < 10 ? '0' + n : n
+	}
+	return d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1) + '-' + pad(d.getUTCDate()) + 'T' + pad(d.getUTCHours()) + ':' + pad(d.getUTCMinutes()) + ':' + pad(d.getUTCSeconds()) + 'Z'
 }
