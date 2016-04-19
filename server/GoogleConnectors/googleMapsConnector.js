cf0645bc550478a785226b9ad4552e08260b3c18
Johanna Latt <johanna.latt@gmx.de>
2016-04-19 16:24:46 +0200

    changed from Google Distance to Google Directions API

--- server/GoogleConnectors/googleMapsConnector.js
+++ server/GoogleConnectors/googleMapsConnector.js
@@ -1,26 +1,16 @@
-var publicConfig = {
-  key: 'AIzaSyBOtSjTEZWmOrMF1x9Tcp0YIalpOo_hX8A',
-  secure:             true, // use https
-};
+var request = require('request');
 
-var GoogleMapsAPI = require('googlemaps');
-var gm = new GoogleMapsAPI( publicConfig );
+var host = 'https://maps.googleapis.com/',
+	path_maps = 'maps/api/',
+	path_distance = 'directions/json';
 
-exports.getDistanceToLocationFromCurrentPosition = function(latitude, longitude, mode, eventData, callback) {
-    var parameters = {
-        "key" : "AIzaSyBOtSjTEZWmOrMF1x9Tcp0YIalpOo_hX8A",
-        "origins" : latitude + ',' + longitude,
-        "destinations" : eventData.location,
-        "departure_time" : new Date(eventData.start.dateTime).getTime(),
-        "mode" : mode       
-    };
-   
-    gm.distance(parameters, function(err, result) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-        }
-        console.log(result.rows[0].elements[0]);
-        callback(result.rows[0].elements[0]);
-    });
+exports.getDistanceToLocationFromCurrentPosition = function (latitude, longitude, mode, eventData, callback) {
+	var departure_time = Math.floor(new Date(eventData.start.dateTime).getTime()) / 1000;
+	var request_url = host + path_maps + path_distance + '?origin=' + latitude + ',' + longitude + '&destination=' + eventData.location + '&departure_time=' + departure_time + '&mode=' + mode + '&key=AIzaSyBOtSjTEZWmOrMF1x9Tcp0YIalpOo_hX8A';
+	request(request_url, function (error, response, body) {
+		directions = JSON.parse(body);
+		//console.log(directions.routes[0].legs[0]);
+		// example output: http://maps.googleapis.com/maps/api/directions/json?origin=52.496,13.358&departure_time=1461158373&destination=52.517,13.341&mode=transit
+		callback(directions.routes[0].legs[0]);
+	});
 }
