85f9416f096d8104811a2ad2880acf373e3418eb
Johanna Latt <johanna.latt@gmx.de>
2016-05-03 16:56:37 +0200

    * add user information to the calendar-object sent to the clock-app * store single events as sub-objects of the 'events'-object of the calendar-object (no longer on the first level of the calendar-object)

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ app.get('/back', function (req, res) {
 // A user connects over socket.io 
 io.on('connection', function (socket) {
 
-    //JSON.stringify(users, null, 4)
-    var id = socket.handshake.query.id;
-    if (users[id] == undefined) users[id] = {};
+	//JSON.stringify(users, null, 4)
+	var id = socket.handshake.query.id;
+	if (users[id] == undefined) users[id] = {};
 
 	socket.on('app - create new user', function () {
 		var googleAuthUrl = oauth2Client.generateAuthUrl({
@@ -70,9 +70,9 @@ io.on('connection', function (socket) {
 
 	socket.on('get calendar', function () {
 		oauth2Client.setCredentials(users[id].tokens)
-        console.log("get calendar");
+		console.log("get calendar");
 		cal.getOrderedFutureCalendarEvents(oauth2Client, function eventListReceived(events) {
-			// Once events are received, use sockets.io to send them to the frontend
+			// Once events are received, use sockets.io to send them to the frontend, 
 			socket.emit('next event', events[0]);
 
 			// Get user mail and send it to the client
@@ -93,9 +93,13 @@ io.on('connection', function (socket) {
 
 	socket.on('app - get users', function () {
 		var stripped_users = {};
-		Object.keys(users).forEach(function(entry) {
-    		stripped_users[entry] = {name: users[entry].name, email: users[entry].email, picture: users[entry].picture};
-  		});
+		Object.keys(users).forEach(function (entry) {
+			stripped_users[entry] = {
+				name: users[entry].name,
+				email: users[entry].email,
+				picture: users[entry].picture
+			};
+		});
 		socket.emit('user list', stripped_users);
 	})
 
@@ -115,19 +119,28 @@ io.on('connection', function (socket) {
 			socket.emit('distance time calculated', data, '🚋');;
 		});
 	});
-    
-    /* ------ CLOCK REQUESTS ------ */
-    socket.on('request calendars', function(day) {    
-        // the clock requests the calendar for a specific day for all logged in users
-        for (var userID in users) {
-            if (userID == undefined) continue;
-            oauth2Client.setCredentials(users[userID].tokens);
-            if (oauth2Client == undefined) console.log('oauth2client undefined in request calendar');
-            cal.getCalendarEventsForOneDay(oauth2Client, day, function(calendar) {
-                if (calendar.length > 0) socket.emit('receive calendar', calendar);         
-            }); 
-        }       
-    });
+
+	/* ------ CLOCK REQUESTS ------ */
+	socket.on('request calendars', function (day) {
+		// the clock requests the calendar for a specific day for all logged in users
+		for (var userID in users) {
+			if (userID == "undefined") continue;
+			oauth2Client.setCredentials(users[userID].tokens);
+			cal.getCalendarEventsForOneDay(oauth2Client, day, function (events) {
+				if (events.length > 0) {
+					// add user information to the calendar before sending it to the clock
+					var calendar = {
+						events: events,
+						name: users[userID].name,
+						email: users[userID].email,
+						picture: users[userID].picture
+					};
+					console.log(JSON.stringify(calendar.events, null, 4));
+					socket.emit('receive calendar', calendar);
+				}
+			});
+		}
+	});
 });
 
 http.listen(8080, function () {
--- server/GoogleConnectors/googleCalendarConnector.js
+++ server/GoogleConnectors/googleCalendarConnector.js
@@ -1,81 +1,93 @@
 module.exports = function (googleCalendar) {
 
-    var module = {};
+	var module = {};
 
-    module.getOrderedFutureCalendarEvents = function (auth, callback) {
-         googleCalendar.events.list({
-            auth: auth,
-            calendarId: 'primary',
-            timeMin: (new Date()).toISOString(),
-            maxResults: 10,
-            singleEvents: true,
-            orderBy: 'startTime'
-          }, function(err, response) {
-            if (err) {
-                console.log('The API returned an error: ' + err);
-                return;
-            }
-            var events = response.items;
-            if (events.length == 0) {
-                console.log('No upcoming events found.');
-            } else {
-                console.log('Upcoming 10 events:');
-                var cleanedUpEvents = [];
-                for (var i = 0; i < events.length; i++) {
-                    var event = events[i];
-                    var start = event.start.dateTime || event.start.date;
-                    console.log('%s - %s', start, event.summary);
-                    if(event.start.dateTime && event.location){
-                        cleanedUpEvents.push(event);
-                    }
-                }
-                if(cleanedUpEvents.length > 0){
-                    callback(cleanedUpEvents);
-                }
-                else {
-                    console.error("No upcoming events with location and start time");
-                }
-            }
-          });
-    }
-    
-    module.getCalendarEventsForOneDay = function(auth, day, callback) {
-        var todayMidnight = new Date(Date.parse(day));
-        todayMidnight.setHours(0,0,0,0);
-        var tomorrowMidnight = new Date(Date.parse(day));;
-        tomorrowMidnight.setDate(tomorrowMidnight.getDate() + 1);
-        tomorrowMidnight.setHours(0,0,0,0);
-        
-        googleCalendar.events.list({
-            auth: auth,
-            calendarId: 'primary',
-            timeMin: todayMidnight.toISOString(),
-            timeMax: tomorrowMidnight.toISOString(),
-            singleEvents: true,
-            orderBy: 'startTime'
-          }, function(err, response) {
-            if (err) {
-                console.log('The API returned an error: ' + err);
-            }
-            var events = response.items;
-            if (events.length == 0) {
-                console.log('No upcoming events found.');
-            } else {
-                var cleanedUpEvents = [];
-                for (var i = 0; i < events.length; i++) {
-                    var event = events[i];
-                    var start = event.start.dateTime || event.start.date;
-                    if(event.start.dateTime){
-                        cleanedUpEvents.push(event);
-                    }
-                }
-                if(cleanedUpEvents.length <= 0){                
-                    console.error('No upcoming events with and start time');
-                }
-                callback(cleanedUpEvents);
-            }
-          });
-    }
+	var maps = require('./googleMapsConnector.js');
 
-    return module;
+	module.getOrderedFutureCalendarEvents = function (auth, callback) {
+		googleCalendar.events.list({
+			auth: auth,
+			calendarId: 'primary',
+			timeMin: (new Date()).toISOString(),
+			maxResults: 10,
+			singleEvents: true,
+			orderBy: 'startTime'
+		}, function (err, response) {
+			if (err) {
+				console.log('The API returned an error: ' + err);
+				return;
+			}
+			var events = response.items;
+			if (events.length == 0) {
+				console.log('No upcoming events found.');
+			} else {
+				console.log('Upcoming 10 events:');
+				var cleanedUpEvents = [];
+
+				for (var i = 0; i < events.length; i++) {
+					var event = events[i];
+					var start = event.start.dateTime || event.start.date;
+					console.log('%s - %s', start, event.summary);
+					if (event.start.dateTime && event.location) {
+						cleanedUpEvents.push(event);
+					}
+				}
+				if (cleanedUpEvents.length > 0) {
+					callback(cleanedUpEvents);
+				} else {
+					console.error("No upcoming events with location and start time");
+				}
+			}
+		});
+	}
+
+	module.getCalendarEventsForOneDay = function (auth, day, callback) {
+		var todayMidnight = new Date(Date.parse(day));
+		todayMidnight.setHours(0, 0, 0, 0);
+		var tomorrowMidnight = new Date(Date.parse(day));;
+		tomorrowMidnight.setDate(tomorrowMidnight.getDate() + 1);
+		tomorrowMidnight.setHours(0, 0, 0, 0);
+
+		googleCalendar.events.list({
+			auth: auth,
+			calendarId: 'primary',
+			timeMin: todayMidnight.toISOString(),
+			timeMax: tomorrowMidnight.toISOString(),
+			singleEvents: true,
+			orderBy: 'startTime'
+		}, function (err, response) {
+			if (err) {
+				console.log('The API returned an error: ' + err);
+			}
+			var events = response.items;
+			if (events.length == 0) {
+				console.log('No upcoming events found.');
+			} else {
+				var cleanedUpEvents = [];
+				for (var i = 0; i < events.length; i++) {
+					var event = events[i];
+					var start = event.start.dateTime || event.start.date;
+
+					// Retrieve all relevant information from the event and add transit data
+					if (event.start.dateTime) {
+						var cleanEvent = {
+							start: new Date(Date.parse(event.start.dateTime)),
+							end: new Date(Date.parse(event.end.dateTime)),
+							title: event.summary
+						};
+						/*maps.addTransitInformationToEvent(cleanEvent, function () {
+							cleanedUpEvents.push(cleanEvent);
+						});*/
+						cleanedUpEvents.push(cleanEvent);
+					}
+				}
+				if (cleanedUpEvents.length <= 0) {
+					console.error('No upcoming events with and start time');
+				}
+				callback(cleanedUpEvents);
+			}
+		});
+	}
+
+	return module;
 };
